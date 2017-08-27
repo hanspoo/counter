6fdc9bab854a984ec1fb724a778e4f84c66d60b4
Hans Poo <hans@welinux.cl>
2017-08-27 11:25:45 -0300

    Refactor separa counter

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Card, Button , Icon } from 'semantic-ui-react'
+import Counter from './components/counter';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-
-    this.state = { counter: 0 }
-  }
-
-  substract() {
-    this.setState({ counter: this.state.counter - 1});
-  }
-  add() {
-    this.setState({ counter: this.state.counter + 1});
-  }
   render() {
     return (
       <div className="App">
-        <Card>
-         <Card.Content>
-          <h1>Counter</h1>
-          <h2>{this.state.counter}</h2>
-
-         </Card.Content>
-         <Card.Content extra>
-           <div className='ui two buttons'>
-             <Button onClick={this.substract.bind(this)}> <Icon name='minus' /></Button>
-             <Button onClick={this.add.bind(this)}> <Icon name='plus' /></Button>
-           </div>
-         </Card.Content>
-       </Card>
-
+        <Counter />
       </div>
     );
   }
